d53d9d0980ec39209e9497d55ee72f9a186e3c4f
Jakub Bednar <jakub.bednar@gmail.com>
2018-07-18 08:31:44 +0200

    Integration tests over Flux docker build

--- compile-and-test.sh
+++ compile-and-test.sh
@@ -10,13 +10,14 @@ DEFAULT_FLUX_DISABLE="false"
 
 INFLUXDB_VERSION="${INFLUXDB_VERSION:-$DEFAULT_INFLUXDB_VERSION}"
 MAVEN_JAVA_VERSION="${MAVEN_JAVA_VERSION:-$DEFAULT_MAVEN_JAVA_VERSION}"
+FLUX_VERSION="nightly"
 FLUX_DISABLE="${FLUX_DISABLE:-$DEFAULT_FLUX_DISABLE}"
 
 echo "Run tests with maven:${MAVEN_JAVA_VERSION} on fluxdb-${INFLUXDB_VERSION} with flux disabled:${FLUX_DISABLE}"
 
 docker kill influxdb || true
 docker rm influxdb || true
-docker pull influxdb:${version}-alpine || true
+docker pull influxdb:${INFLUXDB_VERSION}-alpine || true
 docker run \
           --detach \
           --name influxdb \
@@ -26,35 +27,18 @@ docker run \
           --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
       influxdb:${INFLUXDB_VERSION}-alpine
 
+FLUX_CONTAINER_LINK=""
 if [ ! "$FLUX_DISABLE" == "true" ]; then
 
-    killall fluxd || true
-    
-    # OS Type
-    case "$OSTYPE" in
-      darwin*)
-        archive='platform_nightly_darwin_amd64.tar.gz' ;;
-      linux*)
-        archive="platform_nightly_linux_amd64.tar.gz" ;;
-      msys*)
-        archive="platform_nightly_windows_amd64.zip" ;;
-      *)
-        echo "unknown: $OSTYPE";
-        exit 1 ;;
-    esac
+    docker kill flux || true
+    docker rm flux || true
 
-    # Download/extract the new nightly build
-    baseUrl="https://dl.influxdata.com/flux/nightlies/"
-    if [[ $(find "${archive}" -mtime +1 -print) ]] || [ ! -e "$archive" ] ; then
-      rm -rf platform_nightly*
-      wget --no-use-server-timestamps ${baseUrl}${archive} -O ${archive}
-      tar xvfz ${archive}
-    fi
+    docker pull quay.io/influxdb/flux:${FLUX_VERSION}
 
-    FLUX_IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1`
-    echo "FLUX_IP:" ${FLUX_IP}
     sleep 3
-    platform_nightly_*/fluxd &
+    docker run --detach --name flux --publish 8093:8093 quay.io/influxdb/flux:${FLUX_VERSION}
+
+    FLUX_CONTAINER_LINK="--link=flux"
 fi
 
 test -t 1 && USE_TTY="-t"
@@ -64,12 +48,13 @@ docker run -i ${USE_TTY} --rm  \
       --volume $PWD/.m2:/root/.m2 \
       --workdir /usr/src/mymaven \
       --link=influxdb \
+      ${FLUX_CONTAINER_LINK} \
       --env INFLUXDB_IP=influxdb \
-      --env FLUX_IP=${FLUX_IP} \
-        maven:${MAVEN_JAVA_VERSION} mvn clean install -DFLUX_DISABLE=${FLUX_DISABLE}
+      --env FLUX_IP=flux \
+        maven:${MAVEN_JAVA_VERSION} mvn clean install -rf :influxdb-flux -DFLUX_DISABLE=${FLUX_DISABLE}
 
 docker kill influxdb || true
 
 if [ ! "$FLUX_DISABLE" == "true" ]; then
-    killall fluxd || true
+    docker kill flux || true
 fi
